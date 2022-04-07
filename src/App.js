502240f78ce55e31ff86259696534bc3a1d92d78
Nicolas Wagner <contact@wagner-nicolas.com>
2022-04-07 12:09:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const coin = useRef(null);
+  const [hidden, setHidden] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={() => setHidden(!hidden)}>
+      <h1>Hover over me!</h1>
+      <div class="hammer">
+        <div class="face"></div>
+        <div class="head"></div>
+        <div class="handle"></div>
+      </div>
+      <div id="coin" className={`${hidden ? 'animate' : ''}`} />
+      <h1 className={`${hidden ? 'hidden' : ''}`}>Insert Coin</h1>
+      <div className="piggy">
+        <div className="insert"></div>
+        <div className="cover"></div>
+        <div className="coin-cover"></div>
+        <div className="left-ear">
+          <div className="outset"></div>
+          <div className="inset"></div>
+        </div>
+        <div className="right-ear">
+          <div className="outset"></div>
+          <div className="inset"></div>
+        </div>
+        <div className="eye1"></div>
+        <div className="eye2"></div>
+        <div className="nose">
+          <div className="nostril1"></div>
+          <div className="nostril2"></div>
+        </div>
+        <div className={`mouth ${hidden ? 'smile' : ''}`}></div>
+        <div id="blush" className={`${hidden ? 'animate-blush' : ''}`}></div>
+        <div className="foot foot1"></div>
+        <div className="foot foot2"></div>
+        <div className="foot foot3"></div>
+        <div className="foot foot4"></div>
+      </div>
+      {/* TODO: add rainbow instead of dollars */}
+      <div className="money" id="bill1">
+        <div className="pres"></div>
+      </div>
+      <div className="money" id="bill2">
+        <div className="pres"></div>
+      </div>
+      <div className="money" id="bill3">
+        <div className="pres"></div>
+      </div>
+      <div className="money" id="bill4">
+        <div className="pres"></div>
+      </div>
+      <div className="money" id="bill5">
+        <div className="pres"></div>
+      </div>
+      <div className="money" id="bill6">
+        <div className="pres"></div>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+  const linkElement = screen.getByText(/insert coin/i);
+
   expect(linkElement).toBeInTheDocument();
 });
